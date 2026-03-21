028eb355960634dd75267db6529c81fc58ee24c2
Matthew Yip <e0938566@u.nus.edu>
2026-03-21 18:22:55 +0800

    Draft 1

--- src/ee4308_drone/src/estimator.cpp
+++ src/ee4308_drone/src/estimator.cpp
@@ -164,6 +164,15 @@ namespace ee4308::drone
         }
 
         // if in range, write to Ysonar_, and do the KF correction.
+        Eigen::RowVector2d H;
+        H << 1.0, 0.0;
+
+        const double innovation = Ysonar_ - (H * Xz_)(0);
+        const double innovation_covariance = (H * Pz_ * H.transpose())(0, 0) + var_sonar_;
+        const Eigen::Vector2d K = Pz_ * H.transpose() / innovation_covariance;
+
+        Xz_ = Xz_ + K * innovation;
+        Pz_ = Pz_ - K * H * Pz_;
     }
 
     // ================================ Magnetic sub callback / EKF Correction ========================================
@@ -234,9 +243,17 @@ namespace ee4308::drone
         // dt
         // std::cos(), std::sin()
         // =========
+        Eigen::Matrix2d F;
+        F << 1.0, dt,
+             0.0, 1.0;
 
-        // rewrite or delete the following
-        (void) msg;
+        Eigen::Vector2d W;
+        W << 0.5 * dt * dt, dt;
+
+        const double az = msg.linear_acceleration.z - GRAVITY;
+
+        Xz_ = F * Xz_ + W * az;
+        Pz_ = F * Pz_ * F.transpose() + W * var_imu_z_ * W.transpose();
     }
 
     void Estimator::callbackSubTrueOdom_(const nav_msgs::msg::Odometry msg)
